3c06d84395b1eb75427f0cbfdc6d7474883e247a
abdelawal <abdelawal89@gmail.com>
2025-10-31 04:13:59 +0200

    Add sidebar functionality and styles to CRM layout
    - Introduced sidebar component with collapsible functionality.
    - Added new CSS variables for sidebar colors and styles.
    - Updated HTML structure for sidebar, including navigation items.
    - Removed unused CSS rules from crm-layout styles.
    - Enhanced sidebar styles for better visual consistency.

--- src/app/features/crm-layout/sidebar/sidebar.ts
+++ src/app/features/crm-layout/sidebar/sidebar.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {
+export class Sidebar implements OnInit {
+
+  isSidebarCollapsed = signal<boolean>(false);
+
+  ngOnInit() { }
+
+  toggleSidebar() {
+    this.isSidebarCollapsed.set(!this.isSidebarCollapsed());
+  }
 
 }
