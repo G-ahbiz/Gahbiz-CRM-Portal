28f3eea1d8d1b9c8a3d898305caa1a2aff653c0e
abdelawal <abdelawal89@gmail.com>
2025-11-06 21:04:27 +0200

    Refactor invoice details component for improved data handling and layout
    - Updated invoice-details.html to enhance the presentation of invoice data and improve card titles.
    - Modified invoice-details.css for consistent styling of table headers and cells.
    - Enhanced invoice-details.ts to include services history data and calculations for total quantity and price.

---
+++ src/app/features/crm-dashboard/customers-crm/customers-content/customers-content.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: 'app-customers-content',
+  imports: [RouterOutlet],
+  templateUrl: './customers-content.html',
+  styleUrl: './customers-content.css',
+})
+export class CustomersContent {
+
+}
