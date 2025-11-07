d50a9f4805859ebc8903b1e09d6e376645133fc8
abdelawal <abdelawal89@gmail.com>
2025-11-07 02:37:29 +0200

    Refactor customers-content.html for improved HTML structure
    - Removed unnecessary newline for cleaner code organization and enhanced readability.

---
+++ src/app/features/crm-dashboard/customers-crm/customer-tabel/customer-tabel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-tabel',
+  imports: [],
+  templateUrl: './customer-tabel.html',
+  styleUrl: './customer-tabel.css',
+})
+export class CustomerTabel {
+
+}
