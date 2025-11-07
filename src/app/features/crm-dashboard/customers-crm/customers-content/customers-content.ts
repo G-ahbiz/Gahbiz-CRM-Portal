cc5a757c28b139e7da61286e6abbe91401aca7ce
abdelawal <abdelawal89@gmail.com>
2025-11-07 02:43:22 +0200

    Refactor CustomersCrm and CustomersContent components for improved structure
    - Removed the Customers CRM container and integrated <router-outlet> in customers-crm.html for better routing management.
    - Updated customers-crm.ts to simplify the component by removing unused data handling.
    - Restored the Customers CRM container and tabs header in customers-content.html for proper display.
    - Reintroduced data handling in customers-content.ts to fetch and display customer cards data.

--- src/app/features/crm-dashboard/customers-crm/customers-content/customers-content.ts
+++ src/app/features/crm-dashboard/customers-crm/customers-content/customers-content.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from "@angular/router";
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { AllData } from '../../../../services/all-data';
+import { CardsInterface } from '../../../../services/interfaces/all-interfaces';
+import { TabsHeader } from '../../../../shared/tabs-header/tabs-header';
 
 @Component({
   selector: 'app-customers-content',
-  imports: [RouterOutlet],
+  imports: [TabsHeader, CommonModule],
   templateUrl: './customers-content.html',
   styleUrl: './customers-content.css',
 })
-export class CustomersContent {
+export class CustomersContent implements OnInit {
 
+  cardsData: CardsInterface[] = [];
+
+  constructor(private allData: AllData) { }
+
+  ngOnInit() {
+    this.getCardsData()
+  }
+
+  getCardsData() {
+    this.cardsData = this.allData.getCustomersCardsData();
+  }
 }
--- src/app/features/crm-dashboard/customers-crm/customers-crm.ts
+++ src/app/features/crm-dashboard/customers-crm/customers-crm.ts
@@ -1,26 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { TabsHeader } from "../../../shared/tabs-header/tabs-header";
-import { AllData } from '../../../services/all-data';
-import { CardsInterface } from '../../../services/interfaces/all-interfaces';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-customers-crm',
-  imports: [CommonModule, TabsHeader],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './customers-crm.html',
   styleUrl: './customers-crm.css',
 })
 export class CustomersCrm implements OnInit {
 
-  cardsData: CardsInterface[] = [];
-
-  constructor(private allData: AllData) { }
-
-  ngOnInit() {
-    this.getCardsData()
-  }
-
-  getCardsData() {
-    this.cardsData = this.allData.getCustomersCardsData();
-  }
+  ngOnInit() { }
 }
