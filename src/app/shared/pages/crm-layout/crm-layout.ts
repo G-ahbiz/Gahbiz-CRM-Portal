fbaf64c3cfab0d1a5b3172c367fed72275044189
Mahmoud Waleed <mahmoudwaleednassan@gmail.com>
2025-12-02 20:22:38 +0200

    improve the responsive of layout - sidebar - header

--- src/app/shared/pages/crm-layout/crm-layout.ts
+++ src/app/shared/pages/crm-layout/crm-layout.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from '../../components/header/header';
 import { Sidebar } from '../../components/sidebar/sidebar';
@@ -9,4 +9,55 @@ import { Sidebar } from '../../components/sidebar/sidebar';
   templateUrl: './crm-layout.html',
   styleUrl: './crm-layout.css',
 })
-export class CrmLayout {}
+export class CrmLayout {
+  isMobileScreen = signal<boolean>(false);
+  showMobileBackdrop = signal<boolean>(false);
+  isMobileSidebarOpen = signal<boolean>(false);
+  isSidebarCollapsed = signal<boolean>(false);
+
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    const isMobile = window.innerWidth <= 1024;
+    this.isMobileScreen.set(isMobile);
+
+    if (!isMobile) {
+      this.showMobileBackdrop.set(false);
+      this.isMobileSidebarOpen.set(false);
+    } else {
+      this.showMobileBackdrop.set(false);
+      this.isMobileSidebarOpen.set(false);
+    }
+  }
+
+  isMobile(): boolean {
+    return this.isMobileScreen();
+  }
+
+  toggleMobileSidebar() {
+    this.isMobileSidebarOpen.set(!this.isMobileSidebarOpen());
+    this.showMobileBackdrop.set(this.isMobileSidebarOpen());
+  }
+
+  closeMobileSidebar() {
+    this.isMobileSidebarOpen.set(false);
+    this.showMobileBackdrop.set(false);
+  }
+
+  onSidebarToggled() {
+    if (this.isMobileScreen() && this.isMobileSidebarOpen()) {
+      this.closeMobileSidebar();
+    }
+  }
+
+  toggleSidebarCollapsed() {
+    this.isSidebarCollapsed.set(!this.isSidebarCollapsed());
+  }
+}
