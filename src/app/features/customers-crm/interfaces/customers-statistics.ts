60c6e1e07d8307bbd28f27c469e48d8013ea9fb1
Mahmoud Waleed <mah030577@gmail.com>
2026-02-12 02:02:52 +0200

    Remove user statistics from customer statistics and translations

--- src/app/features/customers-crm/interfaces/customers-statistics.ts
+++ src/app/features/customers-crm/interfaces/customers-statistics.ts
@@ -1,8 +1,6 @@
 export interface CustomersStatistics {
   toltalCustomers: number;
   totlaLeads: number;
-  totalUsers: number;
   customersChangePercentage: number;
   leadsChangePercentage: number;
-  usersChangePercentage: number;
 }
